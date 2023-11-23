9a8a5f857cb0ea5581776c2186c44b09186747bf
Emma Paris <eparis@atlassian.com>
2023-11-23 18:50:32 +0100

    completato esercizi obbligatori completati i primi 2 esercizi extra

--- S3P1-scaricabile.js
+++ S3P1-scaricabile.js
@@ -29,33 +29,77 @@ console.log(risultatoEsercizio)
  Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+function reverseString(stringa) {
+    return stringa.split("").reverse().join("");
+}
+
+let stringaDiProva = reverseString ("Io sono Domenico")
+console.log(stringaDiProva)
+*/
 
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+function upperFirst(stringa) {
+    let array = stringa.split(` `);
+    let maiuscolo = [];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i][0] === array[i][0].toLowerCase()) {
+            maiuscolo.push(array[i][0].toUpperCase() + array[i].slice(1));
+        }
+    }
+    return maiuscolo.join(" ");
+}
+
+let risultatoFinale = upperFirst ("io sono domenico licciardello")
+console.log (risultatoFinale)
+*/
 
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+function giveMeRandom(n) {
+    return (Math.floor(Math.random()*10));
+}
+
+let risultatoFinale = giveMeRandom()
+console.log (risultatoFinale)
+*/
 
 //EXTRA:
 /* ESERCIZIO 1
  Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+function area (l1, l2){
+    let calcolo = l1*l2;
+    return calcolo;
+}
+
+let risultato = area (10,20)
+console.log (risultato)
+*/
 
 /* ESERCIZIO 2
  Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
  Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+function crazyDiff(n) {
+    let calcolo = Math.abs(n - 19);
+    return (calcolo > 19) ?  calcolo*3 : calcolo;
+   }
+
+   let risultato = crazyDiff (50)
+   console.log (risultato)
+*/
 
 /* ESERCIZIO 3
  Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
